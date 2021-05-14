a2f102f608070279ac4460d3b3206c01e2e722cb
varun <59528216+varun1048@users.noreply.github.com>
2021-05-14 22:23:41 +0530

    songs Download

--- MusicPlayer/Masstamilan.py
+++ MusicPlayer/Masstamilan.py
@@ -1,11 +1,29 @@
 from  requests import get as server
 from bs4 import BeautifulSoup as Make
 
-album_name = input("Enter name:")
+album_name = input("Enter name :")
 url=f"https://masstamilan.in/{album_name}"
 
 res = server(url)
 soup = Make(res.text,"html.parser")
-table =soup.find_all(class_="nostyle")
-for x in table:
-    print(x.string)
+
+songName =soup.find_all(class_="nostyle")
+songLink =soup.find_all(class_="dlink anim")
+
+
+filterName = lambda name : "".join((x  for x in name if x != "\t" and x != "\n" ))
+datas = [{"name":filterName(songName[x].string),"link":songLink[x]["href"]} for x in  range(len(songLink)) ]
+
+i = 0
+for x in datas:
+    print(f"{i} \t{x['name']}")
+    i += 1
+
+def Download(fileName,url):
+    res = server(url)
+    with open(fileName+".mp3",'wb') as file:
+        file.write(res.content)
+        print("Done")
+
+num = int(input("song number to download :"))
+Download(str(album_name+"_"+datas[num]["name"]),datas[num]["link"])
