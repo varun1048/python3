930b562a0308a0e6d943bc5613f02be5e2d9b6e4
varun <59528216+varun1048@users.noreply.github.com>
2021-05-14 23:11:20 +0530

    Download  tamil songs

--- MusicPlayer/Masstamilan.py
+++ MusicPlayer/Masstamilan.py
@@ -1,29 +1,36 @@
 from  requests import get as server
 from bs4 import BeautifulSoup as Make
 
-album_name = input("Enter name :")
-url=f"https://masstamilan.in/{album_name}"
+class Masstamilan:
+    def __init__(self,inner):
+        self.album_name = inner
+        url=f"https://masstamilan.in/{self.album_name}"
+        res = server(url)
+        soup = Make(res.text,"html.parser")
+        
+        songName =soup.find_all(class_="nostyle")
+        songLink =soup.find_all(class_="dlink anim")
+        filterName = lambda name : "".join((x  for x in name if x != "\t" and x != "\n" ))
+        self.datas = [{"name":filterName(songName[x].string),"link":songLink[x]["href"]} for x in  range(len(songLink))]
+            
 
-res = server(url)
-soup = Make(res.text,"html.parser")
+    def displaySongs(self):
+        i = 0
+        for x in self.datas:
+            print(f"{i} \t{x['name']}")
+            i += 1
 
-songName =soup.find_all(class_="nostyle")
-songLink =soup.find_all(class_="dlink anim")
+    def (self):
+        num = int(input("song number to download :"))
+        if num in range(len(self.datas)):
+            fileName =str(self.album_name+"_"+self.datas[num]["name"])
+            res = server(self.datas[num]["link"])
+            with open(fileName+".mp3",'wb') as file:
+                file.write(res.content)
+            print("Done")
+        else:
+            print("invalied input")
 
-
-filterName = lambda name : "".join((x  for x in name if x != "\t" and x != "\n" ))
-datas = [{"name":filterName(songName[x].string),"link":songLink[x]["href"]} for x in  range(len(songLink)) ]
-
-i = 0
-for x in datas:
-    print(f"{i} \t{x['name']}")
-    i += 1
-
-def Download(fileName,url):
-    res = server(url)
-    with open(fileName+".mp3",'wb') as file:
-        file.write(res.content)
-        print("Done")
-
-num = int(input("song number to download :"))
-Download(str(album_name+"_"+datas[num]["name"]),datas[num]["link"])
+call  = Masstamilan(input("Album name Tamil :"))
+call.displaySongs()
+call.Download()
