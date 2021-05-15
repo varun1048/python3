a9fd2f4e593422b025dd0dc61e4147551f5e95be
varun <59528216+varun1048@users.noreply.github.com>
2021-05-15 12:51:47 +0530

    python

--- MusicPlayer/Masstamilan.py
+++ MusicPlayer/Masstamilan.py
@@ -2,35 +2,71 @@ from  requests import get as server
 from bs4 import BeautifulSoup as Make
 
 class Masstamilan:
+
     def __init__(self,inner):
-        self.album_name = inner
-        url=f"https://masstamilan.in/{self.album_name}"
+        album = inner.replace(" ","-")
+        url=f"https://masstamilan.in/{album}"
         res = server(url)
-        soup = Make(res.text,"html.parser")
+        self.soup = Make(res.text,"html.parser")
+        self.album_name = self._filterName(self.soup.title.string)
+        # print(self.album_name)
         
-        songName =soup.find_all(class_="nostyle")
-        songLink =soup.find_all(class_="dlink anim")
-        filterName = lambda name : "".join((x  for x in name if x != "\t" and x != "\n" ))
-        self.datas = [{"name":filterName(songName[x].string),"link":songLink[x]["href"]} for x in  range(len(songLink))]
-            
+        if self.soup.title.string == "Masstamilan | High Quality Tamil Mp3 Songs Free Download":
+            notvalied = """\tEnnama neenga ippadi panreengale ma. Olunga address ah type pannunga ma."""
+            print(notvalied)
+        else:
+            songName =self.soup.find_all(class_="nostyle")
+            songLink =self.soup.find_all(class_="dlink anim")
+            filterName = lambda name : "".join((x  for x in name if x != "\t" and x != "\n" ))
+            self.datas = [{"name":filterName(songName[x].string),"link":songLink[x]["href"]} for x in  range(len(songLink))]
+
+    def albumInfo(self):
+        keys=["Starring","Director","Music"]
+        info = dict.fromkeys(keys,"i")
+        # print(info[0])
+        out=self.soup.find(class_="info").p.get_text("|",strip=True).split("|")  #"",strip=True
+        out = [out[x] for x in range(len(out)-5) if x%2 != 0]
+        # i =0
+        # result = {x : out[0] for x in info  }
+        print(info)
+        
+
+    def _filterName(self,name):
+        result = ""
+        for  x in str(name):
+            if x == ")":
+                result += ")"
+                break 
+            result += x
+        return result
+    
+
 
     def displaySongs(self):
         i = 0
-        for x in self.datas:
-            print(f"{i} \t{x['name']}")
-            i += 1
+        try:
+            for x in self.datas:
+                print(f"{i} \t{x['name']}")
+                i += 1
+        except  Exception as err:
+            print(f"Oops!\t{err.__class__} occurred. at displaySongs")
 
-    def (self):
-        num = int(input("song number to download :"))
-        if num in range(len(self.datas)):
-            fileName =str(self.album_name+"_"+self.datas[num]["name"])
-            res = server(self.datas[num]["link"])
-            with open(fileName+".mp3",'wb') as file:
-                file.write(res.content)
-            print("Done")
+    def Download(self):
+        try:
+            num = int(input("song number to download :"))
+        except  Exception as err:
+                print("Oops!\t", err.__class__, "occurred. at Download")
         else:
-            print("invalied input")
+            if num in range(len(self.datas)):
 
-call  = Masstamilan(input("Album name Tamil :"))
-call.displaySongs()
-call.Download()
+                fileName =str(self.album_name+"_"+self.datas[num]["name"])
+                res = server(self.datas[num]["link"])
+                with open(fileName+".mp3",'wb') as file:
+                    file.write(res.content)
+                print("Download Done")
+            else:
+                print("invalied input")
+
+# call  = Masstamilan(input("Album name Tamil :"))
+# call.displaySongs()
+# call.Download()
---
+++ MusicPlayer/gui.py
@@ -0,0 +1,8 @@
+from Masstamilan import Masstamilan
+
+# inner = input("Album name :")
+inner = "master"
+songs  = Masstamilan(inner)
+# songs.displaySongs()
+# songs.Download()
+songs.albumInfo()
